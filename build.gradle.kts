cb717b9523dee5657c8744ff14046ac2c25836b0
Estela Alves. <123739850+estelalm@users.noreply.github.com>
2024-09-14 17:09:40 -0300

    14 set. 2024 - dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,94 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    kotlin("plugin.serialization") version "2.0.20"
+}
+
+android {
+    namespace = "br.senai.sp.jandira.touccanmobile_user"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "br.senai.sp.jandira.touccanmobile_user"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(kotlin("script-runtime"))
+
+    //RETROFIT
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+
+    //GSON
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    //COIL
+    implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation(kotlin("script-runtime"))
+
+
+    //JSON SERIALIZER
+    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+
+    //NAVIGATION
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
 }
